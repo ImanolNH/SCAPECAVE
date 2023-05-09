fd65e10d071b1d31be0861f498c6980e2fbdaff2
ImanolNH <imanolnavarrete21@gmail.com>
2023-05-09 19:41:31 +0200

    Revert "Esqueleto arreglado y munición muestra mensaje"
    This reverts commit 3738f8ec1d9e8c36cb984a48977489f15a09aff3.

--- SCAPE-CAVE/Assets/Scripts/Weapon/Gun.cs
+++ SCAPE-CAVE/Assets/Scripts/Weapon/Gun.cs
@@ -17,63 +17,29 @@ public class Gun : MonoBehaviour
 
     public TMP_Text municion;
     public TMP_Text cargador;
-
-    public TMP_Text mensaje;
-
-    private bool mensajeMostrado = false;
     //public estadisticas municion;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1"))
         {
-            if (GameManager.Instance.gunAmmo == 0 && GameManager.Instance.ammoCargador ==0) {
+            if(Time.time>shotRateTime && GameManager.Instance.gunAmmo>0){
+                GameManager.Instance.gunAmmo--;
 
-                if (!mensajeMostrado)
-                {
-                    mensaje.text = "No hay munición";
-                    StartCoroutine(EsperarYEliminarMensaje(2f));
-                    mensajeMostrado = true;
-                }
+                string ammo = GameManager.Instance.gunAmmo.ToString();
+                municion.text = ammo;
 
-            }
-            else if(GameManager.Instance.gunAmmo == 0 && GameManager.Instance.ammoCargador > 0)
-            {
-                if (!mensajeMostrado)
-                {
-                    mensaje.text = "Debes recargar munición";
-                    StartCoroutine(EsperarYEliminarMensaje(2f));
-                    mensajeMostrado = true;
-                }
-            }
-            else
-            {
-                if (Time.time > shotRateTime && GameManager.Instance.gunAmmo > 0)
-                {
-                    GameManager.Instance.gunAmmo--;
+                GameObject newBullet;
 
-                    string ammo = GameManager.Instance.gunAmmo.ToString();
-                    municion.text = ammo;
+                newBullet=Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
 
-                    GameObject newBullet;
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*shotForce);
+                
+                shotRateTime=Time.time+shotRate;
 
-                    newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
-
-                    newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
-
-                    shotRateTime = Time.time + shotRate;
-
-                    Destroy(newBullet, 3);
-                }
-
+                Destroy(newBullet, 3);
             }
             
         }
-    }
-    IEnumerator EsperarYEliminarMensaje(float tiempo)
-    {
-        yield return new WaitForSeconds(tiempo);
-        mensaje.text = "";
-        mensajeMostrado = false;
     }
 }
